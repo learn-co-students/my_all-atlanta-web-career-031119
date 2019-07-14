980b346850ed944505d133795040218b2dee8319
github username <github email address>
2019-07-14 17:52:14 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
-require 'pry'
+require 'byebug'
 
 def my_all?(collection)
-
+    i=0
+    res=[]
+    while i<collection.length
+        res<<yield(collection[i])
+        i+=1
+    end
+    !res.include?(false)
 end
